63d58e8a539d943e15ed865bf4ea3406a8970eed
yhx1 <invalid@nonexist.com>
2019-06-13 22:27:10 -0700

    Load Balancer Configuration and Support

--- middleware/src/main/java/edu/ics/uci/cs237Application.java
+++ middleware/src/main/java/edu/ics/uci/cs237Application.java
@@ -13,9 +13,13 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.websockets.WebsocketBundle;
 import okhttp3.OkHttpClient;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.jdbi.v3.core.Jdbi;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.websocket.server.ServerEndpointConfig;
+import java.util.EnumSet;
 
 public class cs237Application extends Application<cs237Configuration> {
 
@@ -41,6 +45,15 @@ public class cs237Application extends Application<cs237Configuration> {
     @Override
     public void run(final cs237Configuration configuration,
                     final Environment environment) {
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 
         final JdbiFactory factory = new JdbiFactory();
         final Jdbi jdbi = factory.build(environment, configuration.getDataSourceFactory(), "postgresql");
